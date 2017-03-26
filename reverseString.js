51651a8d38d73f8cd0f8d64d033d44d900abfa8c
zoro-lee <honggiahieu@gmail.com>
2017-03-26 11:58:37 +0700

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,6 @@
 function reverseString (str){
 	var arr = str.split(''); // arr = ["h","e","l","l","o"]
 	arr.reverse(); // arr = ["o","l","l","e","h"]
-	return arr.join(''); // arr = olleh
+	var rev = arr.join(''); // olleh
+	return rev;
 };
